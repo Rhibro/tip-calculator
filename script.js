0303ba4501075be0c0dc013530935b1ec52c01ab
Rhibro <111956763+Rhibro@users.noreply.github.com>
2022-12-01 15:59:59 +0100

    trying to add function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let billPayment = 560;
+/*const billPayment = 560;
 let percentAmount;
 let percent; 
 
@@ -14,7 +14,33 @@ if (billPayment > 300) {
 
 percentAmount = (percent / 100) * billPayment;
 
-let total = percentAmount + billPayment;
+const total = percentAmount + billPayment;
+
+console.log(`The total amount of the bill is ${billPayment}. The tip % is therefore 
+
+${percent} and the total amount to pay including tip is ${total}`)*/
+
+const billPayment = 560;
+let percentAmount;
+let percent; 
+
+
+
+function calcTip(billPayment, percentAmount, percent){
+    if (billPayment > 300) {
+
+        percent = 15 
+    
+    } else if (billPayment < 300) {
+        
+        percent = 10 
+    
+    }
+    return percentAmount = ((percent / 100) * billPayment);
+    total = percentAmount + billPayment;
+}
+
+total = percentAmount + billPayment;
 
 console.log(`The total amount of the bill is ${billPayment}. The tip % is therefore 
 
